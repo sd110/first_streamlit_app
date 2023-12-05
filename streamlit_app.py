7728c7f9d78b10cce07616b5f8639cdb3f0837c4
sd110 <sd_110_1995@hotmail.com>
2023-12-05 20:54:27 +0100

    Update streamlit_app.py
    fixed bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents new healthy diner')
 
 streamlit.header( 'Breakfast Menu' )
-streamlit.text( '🥣 Omega 3 & Blueberry Oatmeal
+streamlit.text( '🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text( '🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text( '🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avicado Toast')
