fee21ccee09bacafb4259ab7a5f55ef802dd39b5
sd110 <sd_110_1995@hotmail.com>
2023-12-14 20:09:10 +0100

    Update streamlit_app.py
    removed raw json text and changed to kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 #L9
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
